b3c8ec96e01ff162b09c529d25a5551e90725f58
Tenji Tembo <tenji240@gmail.com>
2018-01-18 12:39:33 -0500

    more colors, border divider

--- assets/js/shift.js
+++ assets/js/shift.js
@@ -1,20 +1,31 @@
-var colors = ['rgba(51,153,255,0.5)','rgba(58,123,213,0.5)','rgba(28,181,224,0.5)','rgba(86,204,242,0.5)','rgba(47,128,237,0.5)','rgba(47,128,237,0.5)'];
+var colors = [
+  'rgba(52,194,219,0.5)',
+  'rgba(52,180,219,0.5)',
+  'rgba(52,166,219,0.5)',
+  'rgba(52,152,219,0.5)',
+  'rgba(52,138,219,0.5)',
+  'rgba(52,124,219,0.5)',
+  'rgba(52,110,219,0.5)',
+  'rgba(52,208,219,0.5)',
+  'rgba(52,219,216,0.5)',
+  'rgba(52,219,202,0.5)',
+];
 
-function getRandom(){
+function getRandom() {
   return Math.floor(Math.random() * colors.length);
 }
 
-function returnLinearGradient(){
+function returnLinearGradient() {
   return colors[getRandom()];
 }
 
-function main(){
-  setInterval(function(){
+function main() {
+  setInterval(function() {
     $('.background-tint').css('background-color', returnLinearGradient());
   }, 2000);
 }
 
-$(document).ready(function(){
-  console.log('ready');
+$(document).ready(function() {
+  //console.log('ready');
   main();
-})
+});
